3227febe7d30ebb5697a62fad2e83bb2751c5d49
jameslfc19 <jameslfc19@gmail.com>
2014-06-15 18:09:57 +0100

    Initial commit

---
+++ android/src/com/jameslfc19/tiltshape/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jameslfc19.tiltshape.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jameslfc19.tiltshape.TiltShape;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TiltShape(), config);
+	}
+}
---
+++ desktop/src/com/jameslfc19/tiltshape/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jameslfc19.tiltshape.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jameslfc19.tiltshape.TiltShape;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TiltShape(), config);
+	}
+}
